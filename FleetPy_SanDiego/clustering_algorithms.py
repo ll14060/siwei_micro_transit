5042270597fa81b5a840171017e2e38c819ea98d
ll14060 <44488709+ll14060@users.noreply.github.com>
2025-10-07 20:24:20 -0700

    Finished clustering v1

--- FleetPy_SanDiego/clustering_algorithms.py
+++ FleetPy_SanDiego/clustering_algorithms.py
@@ -1,8 +1,27 @@
-import network_algorithms
+import os
+import math
+import random
+import csv
+import datetime
+from collections import defaultdict, OrderedDict, deque  # deque if you want later
+from itertools import islice
+from math import radians, cos, sin, asin, sqrt
+import heapq
+
+import numpy as np
+import pandas as pd
+import scipy
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+import networkx as nx
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
-import numpy as np
+
+from network_algorithms import read_super_network, read_request, dijsktra_all_to_all
+
+from setuptools import setup
+from Cython.Build import cythonize
 
 def normalized_sensitivities(agent):
     total = (
@@ -40,9 +59,44 @@ def normalized_sensitivities(agent):
 
 def clustering(agent_list):
     agents_w_sens = {}
+    range_n_clusters= [2,3,4,5,6]
+
     for agent in agent_list:
         agents_w_sens[agent] = normalized_sensitivities(agent)
-        
+
+    kmeans_array = np.array(list(agents_w_sens.values()))
+    best_n = get_best_n(range_n_clusters, kmeans_array)
+    kmeans = KMeans(n_clusters=best_n, n_init="auto")
+    labels = kmeans.fit_predict(kmeans_array)
+
+    clustered_agents = {agent: label for agent, label in zip(agents_w_sens.keys(), labels)}
+    centroids = kmeans.cluster_centers_
+
+    cluster_to_agents = defaultdict(list)
+    for label, agent in zip(agents_w_sens.keys(), labels):
+        cluster_to_agents[label].append(agent)
+
+    clusters_info = []
+    for cluster_id, agents in cluster_to_agents.items():
+        clusters_info.append({
+            "cluster_id": cluster_id,
+            "centroid": centroids[cluster_id],
+            "agents": agents
+        })
+
+    return clusters_info, kmeans.inertia_
+
+#Finds best # of clusters by finding n with the highest silhouette score
+def get_best_n(range_n_clusters, sens):
+    silhouette_scores = {}
+    for n_clusters in range_n_clusters:
+        clusterer = KMeans(n_clusters=n_clusters, n_init = "auto")
+        cluster_labels = clusterer.fit_predict(sens)
+        silhouette_scores[n_clusters] = silhouette_score(sens, cluster_labels)
+    return max(silhouette_scores, key = silhouette_scores.get)
+
+
+
 
 
         
